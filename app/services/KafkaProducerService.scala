cc88612bfde20d8f22ca682963a31c054f60ada7
Rohit Saxena <aerohit.saxena@gmail.com>
2016-11-06 00:15:50 +0100

    A kafka log producer.

---
+++ app/services/KafkaProducerService.scala
@@ -0,0 +1,30 @@
+package services
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+trait LogQueue {
+  def publish(topic: String, message: String)
+}
+
+class KafkaProducerService(props: Properties) extends LogQueue {
+  private val producer = new KafkaProducer[String, String](props)
+
+  override def publish(topic: String, message: String) = {
+    val record = new ProducerRecord[String, String](topic, message)
+    producer.send(record)
+  }
+}
+
+object KafkaProducerService {
+  // TODO: read the configuration for a config file
+  def apply() = {
+    val  props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+    new KafkaProducerService(props)
+  }
+}
--- build.sbt
+++ build.sbt
@@ -6,10 +6,16 @@ lazy val commonSettings = Seq(
   scalaVersion := "2.11.7"
 )
 
-lazy val root = (project in file(".")).enablePlugins(PlayScala).settings(commonSettings: _*)
+resolvers += Resolver.bintrayRepo("cakesolutions", "maven")
 
 libraryDependencies ++= Seq(
+  // https://mvnrepository.com/artifact/org.apache.kafka/kafka_2.11
+//  libraryDependencies += "org.apache.kafka" % "kafka_2.11" % "0.10.1.0"
+  "net.cakesolutions" %% "scala-kafka-client" % "0.10.0.0"
+//  "org.apache.kafka" %% "kafka-clients" % "0.10.1.0"
 )
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala).settings(commonSettings: _*)
 
 lazy val requestsProcessor = project.in(file("request-processor")).settings(commonSettings: _*)
 
