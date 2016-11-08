7542e828abbeaf8ef5c1bf6809bb31744012a436
Rohit Saxena <aerohit.saxena@gmail.com>
2016-11-08 02:46:14 +0100

    Simplified the transformation task too.

--- request-processor/src/main/scala/services/UriRequestToRichObjectTransformer.scala
+++ request-processor/src/main/scala/services/UriRequestToRichObjectTransformer.scala
@@ -8,9 +8,6 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-
 object UriRequestToRichObjectTransformer extends App {
   implicit val system = ActorSystem("Request-Processor")
   implicit val materializer = ActorMaterializer()
@@ -24,10 +21,14 @@ object UriRequestToRichObjectTransformer extends App {
     .withBootstrapServers("localhost:9092")
 
   Consumer.plainSource(consumerSettings, Subscriptions.topics("madebar"))
-    .mapAsync(1) { r =>
-      println(s"Read record ${r.value()}")
-      // TODO: there shouldn't be a need to call get here, check if filter is possible
-      val geo = GeoCoordinatesService.enrich(r.value()).get
-      Future(new ProducerRecord[String, String]("barmade", geo.serializeToString()))
-    }.runWith(Producer.plainSink(producerSettings))
+    .map(r => GeoCoordinatesService.enrich(r.value()))
+    .collect { case Some(geo) => geo }
+    .map(convertToRecord)
+    .runWith(Producer.plainSink(producerSettings))
+
+  def convertToRecord(geo: GeoCoordinate): ProducerRecord[String, String] = {
+    val serialized: String = geo.serializeToString()
+    println(s"The Serialized: $serialized")
+    new ProducerRecord[String, String]("barmade", serialized)
+  }
 }
