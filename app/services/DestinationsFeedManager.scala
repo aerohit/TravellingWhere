c167b93c007f20e0ffe450aa4a0bfbec4d6bfefa
Rohit Saxena <aerohit.saxena@gmail.com>
2016-11-08 01:34:58 +0100

    DestinationsFeedManager also implements SubscribableActor trait.

--- app/services/DestinationsFeedManager.scala
+++ app/services/DestinationsFeedManager.scala
@@ -1,19 +1,20 @@
 package services
 
+import actors.SubscribableActor
 import akka.actor._
-import play.api.Logger
 import play.api.libs.json.JsValue
 
-import scala.collection.mutable.ArrayBuffer
-
 sealed trait DestinationsFeedProtocol
+
 case object SubscribeToFeed extends DestinationsFeedProtocol
+
 case object UnSubscribeToFeed extends DestinationsFeedProtocol
+
 case class SubscriptionFeed(json: JsValue) extends DestinationsFeedProtocol
 
-class DestinationsFeedManager extends Actor {
-  private val remote = context.actorSelection("akka.tcp://DestinationsFeedManager@127.0.0.1:5150/user/DestinationFeedAggregatorActor")
-  private val subscribers = new ArrayBuffer[ActorRef]()
+class DestinationsFeedManager extends Actor with SubscribableActor[SubscriptionFeed] {
+  private val remote =
+    context.actorSelection("akka.tcp://DestinationsFeedManager@127.0.0.1:5150/user/DestinationFeedAggregatorActor")
 
   override def preStart() = {
     remote ! "subscribe"
@@ -29,28 +30,11 @@ class DestinationsFeedManager extends Actor {
       unsubscribe(subscriber)
 
     case jsonResponse: JsValue =>
-      notifySubscribers(jsonResponse)
+      notifySubscribers(SubscriptionFeed(jsonResponse))
 
     case other =>
       // TODO: Log it in a warningful way
       println(s"DestinationsFeedManager received unknown: '$other'")
-  }
-
-  private def subscribe(out: ActorRef): Unit = {
-    Logger.info("Subscribing to feed.")
-    subscribers += out
-  }
-
-  private def unsubscribe(subscriber: ActorRef): Unit = {
-    val index = subscribers.indexWhere(_ == subscriber)
-    if (index > 0) {
-      subscribers.remove(index)
-      Logger.info("Unsubscribed from feed")
-    }
-  }
-
-  private def notifySubscribers(json: JsValue) =  {
-    subscribers.foreach(s => s ! SubscriptionFeed(json))
   }
 }
 
